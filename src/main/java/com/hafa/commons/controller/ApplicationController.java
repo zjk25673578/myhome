2825eb931db556348d9567e8edb5aa46cfaf0070
zhaojk <25673578@qq.com>
2018-11-26 00:11:15 +0800

    首页

--- src/main/java/com/hafa/commons/controller/ApplicationController.java
+++ src/main/java/com/hafa/commons/controller/ApplicationController.java
@@ -6,20 +6,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ApplicationController {
 
+    /* blog start */
+    @RequestMapping("/index")
+    public String index() {
+        return "blog/index";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "blog/about";
+    }
+
+    @RequestMapping("/album")
+    public String album() {
+        return "blog/album";
+    }
+
+    @RequestMapping("/details")
+    public String details() {
+        return "blog/details";
+    }
+
+    @RequestMapping("/leacots")
+    public String leacots() {
+        return "blog/leacots";
+    }
+
+    @RequestMapping("/whisper")
+    public String whisper() {
+        return "blog/whisper";
+    }
+    /* blog end */
+
+    /* record start */
     @RequestMapping("/login")
     public String toLogin() {
         return "record/login";
-    }
-
-    @RequestMapping("/index")
-    public String index() {
-        return "record/index";
     }
 
     @RequestMapping("/welcome")
     public String welcome() {
         return "record/welcome";
     }
+    /* record end */
 
     @RequestMapping("/error404")
     public String error404() {
@@ -29,10 +58,5 @@ public class ApplicationController {
     @RequestMapping("/error500")
     public String error500() {
         return "jsp/500";
-    }
-
-    @RequestMapping("/test")
-    public String test() {
-        return "aabbcc/test";
     }
 }
